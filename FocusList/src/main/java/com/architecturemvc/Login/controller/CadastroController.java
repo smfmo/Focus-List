0231278410ca079e20ee6486d6bd1775b04c3843
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-05-16 17:04:32 -0300

    alterações

---
+++ FocusList/src/main/java/com/architecturemvc/Login/controller/CadastroController.java
@@ -0,0 +1,7 @@
+package com.architecturemvc.Login.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CadastroController {
+}
---
+++ FocusList/src/main/java/com/architecturemvc/Login/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.architecturemvc.Login.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class LoginController {
+
+}
